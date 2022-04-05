65d348f2bcd27479f2e37322ccbc7205042a454d
Mustafa Ahmed <mustafaahmed41@gmail.com>
2022-04-05 17:19:12 +0530

    initialization

---
+++ src/test/java/com/testby/bypretest/ByPretestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testby.bypretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByPretestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
